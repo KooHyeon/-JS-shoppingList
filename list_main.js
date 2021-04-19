8b62683bcb7ace4717e63245ce4cc88bd14455ba
koohyeon <oldandnow9@gmail.com>
2021-04-19 14:57:08 +0900

    JS: Delete Enter Event and Add Input Button Event

--- list_main.js
+++ list_main.js
@@ -2,6 +2,7 @@
 
 const list = document.querySelector('.list');
 const inputField = document.querySelector('.input-field');
+const inputBtn = document.querySelector('.input-btn');
 const normalRunji = document.querySelector('.normal');
 const angryRunji = document.querySelector('.angry');
 const items = document.querySelector('.list-items');
@@ -12,25 +13,24 @@ form.addEventListener('submit', (e)=>{
     e.preventDefault();
 });
 
+inputBtn.addEventListener('click', () => {
+    addList();
+})
 
-inputField.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) {
-        addList(e);
-    }
-});
-
-function addList(e){
+function addList(){
     const listItem = document.createElement('div');
     const itemText = document.createElement('li');
     const deleteBtn = document.createElement('button');
-    itemText.textContent = e.target.value;
+    itemText.textContent = inputField.value;
     deleteBtn.classList.add('delete-btn');
     items.appendChild(listItem);
     listItem.appendChild(deleteBtn);
     listItem.appendChild(itemText);
-    e.target.value = "";
+    inputField.value = "";
 
     deleteBtn.addEventListener('click', deleteItem);
+
+    normalRunji.style.display = hidden;
 }
 
 function deleteItem (e) {
