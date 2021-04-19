95f43e0ddfe303069d81244f8eb59c4dae715174
koohyeon <oldandnow9@gmail.com>
2021-04-19 14:47:01 +0900

    JS: Remove Item

--- list_main.js
+++ list_main.js
@@ -7,6 +7,7 @@ const angryRunji = document.querySelector('.angry');
 const items = document.querySelector('.list-items');
 const form = document.querySelector('form');
 
+
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 });
@@ -28,5 +29,16 @@ function addList(e){
     listItem.appendChild(deleteBtn);
     listItem.appendChild(itemText);
     e.target.value = "";
+
+    deleteBtn.addEventListener('click', deleteItem);
+}
+
+function deleteItem (e) {
+    const btn = e.target;
+    const item = btn.nextSibling;
+    const list = btn.parentNode;
+    list.removeChild(item);
+    list.removeChild(btn);
+    list.remove();
 }
 
